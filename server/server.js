f44552cd0528b82db44f5a9fee551ee7dd98e139
ADRIAN <adrysanchezr@gmail.com>
2019-03-28 18:23:34 +0100

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('./config/config');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/user', function(req, res) {
+    res.json('getUser');
+});
+
+app.post('/user', function(req, res) {
+
+    let body = req.body;
+
+    if (body.name === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            message: 'Name its necessary'
+        });
+
+    } else {
+
+        res.json({
+            person: body
+        });
+
+    }
+
+});
+
+app.put('/user/:id', function(req, res) {
+
+    let id = req.params.id;
+
+    res.json({
+        id
+    });
+
+});
+
+app.delete('/user', function(req, res) {
+    res.json('deleteUser');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando en el puerto: ', process.env.PORT);
+});
